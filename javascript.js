52ddc3442dfc504b7879f58999deafd973696353
CodBen <junes828@gmail.com>
2021-02-10 14:03:53 +0100

    stockage api

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 console.log('Mon JS va rocker !')
+
+// ---------------
+
+let ville = "Marseille";
+//Je préselectionne ma ville 
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=30d9c893acee6948c65f5c233f90601c&units=metric';
+
+// je stocke mon api 
+
