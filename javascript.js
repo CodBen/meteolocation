3e1c11cdb9bc3584a4aefdf4fe6d2489ea93394f
CodBen <junes828@gmail.com>
2021-02-13 20:38:52 +0100

    géolocalisation

--- javascript.js
+++ javascript.js
@@ -2,10 +2,44 @@ console.log('Mon JS va rocker !')
 
 // ---------------
 
-let ville = "Marseille";
+let ville;
 //Je préselectionne ma ville(de base avant modification)
 
-recupererMeteo(ville)
+if("geolocation" in navigator) {
+    navigator.geolocation.watchPosition((position) => {
+    
+
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=30d9c893acee6948c65f5c233f90601c&units=metric';
+        let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    //
+    requete.responseType = 'json';
+
+    requete.send();
+
+    requete.onload = () => {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200 ) {
+                // verification que tout est OK
+                let reponse = requete.response;
+                console.log(reponse);
+                //CHECK
+                let ville = reponse.name;
+                let temperature = reponse.main.temp
+                document.querySelector('#ville').textContent=ville; // SELECTEUR
+                document.querySelector('#temperature_label').textContent=temperature // SELECTEUR
+            } else {
+                alert('Bien évidement, ça ne fonctionne pas !');
+            }
+        }
+    }
+
+    });
+
+} else {
+    ville ='marseille';
+    recupererMeteo(ville);
+}
 // Je passe ma function avec le parametre ville pour qu'il soit dynamique
 let changerDeVille = document.querySelector('#changer');
 // je sélectionne le bouton
